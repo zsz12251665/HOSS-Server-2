ffd6e862207cab622bac114a1b3d8d0cb4d6ec4e
zsz12251665 <zsz12251665@hotmail.com>
2021-03-05 15:21:28 +0800

    Add accesser for static resources

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ const cors = require('cors');
 const multipart = require('connect-multiparty');
 
 var app = express();
+
 app.use(cors()); // 允许跨域请求
+
 app.use(express.urlencoded({ extended: false })); // 解析 application/x-www-form-urlencoded
 app.use(express.json()); // 解析 application/json
 app.use(multipart()); // 解析 multipart/form-data
+
+app.use(express.static('static')); // 访问静态资源
 
 app.use('/api', require('./api/router')); // 设定 /api 的路由器
 
